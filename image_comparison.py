8fe89e9e69dc84eceed7822259394f0df620f718
haasj22 <49049551+haasj22@users.noreply.github.com>
2020-10-16 19:55:18 -0700

    Add files via upload

--- image_comparison.py
+++ image_comparison.py
@@ -7,19 +7,24 @@ from PIL import Image
 import os
 
 for filename in os.listdir('cropped_frames'):
-    first_im_read=[]
+    first_im_read=None
     if not filename.startswith('.'):
         for filenameception in os.listdir('cropped_frames/' + str(filename)):
             if filenameception.endswith('.jpg'):
-                if(first_im_read == []):
-                    first_im_read = cv2.imread("cropped_frames/" + str(filename) + '/' + str(filenameception))
+                if(first_im_read is None):
+                    first_im_read = cv2.imread("cropped_frames/" + str(filename) + '/' + str(filenameception), 0)
+                    print("First" + str(first_im_read))
                     if not os.path.exists('different_frames'):
                         os.makedirs('different_frames')
                     if not os.path.exists("different_frames/" + "D" + str(filename)):
                         os.makedirs("different_frames/" + "D" + str(filename))
-                    cv2.imwrite("different_frames/" + "D" + str(filename) + '/' + str(filenameception), first_im_read)
+                    #cv2.imwrite("different_frames/" + "D" + str(filename) + '/' + str(filenameception), first_im_read)
                 else:
-                    new_im = cv2.imread("cropped_frames/" + str(filename) + '/' + str(filenameception))
+                    print("First" + str(first_im_read))
+                    new_im = cv2.imread("cropped_frames/" + str(filename) + '/' + str(filenameception), 0)
+                    print("cropped_frames/" + str(filename) + '/' + str(filenameception))
+                    print("Second" + str(new_im))
                     error = measure.compare_ssim(first_im_read, new_im)
+                    print(error)
                     if error < 0.75:
-                        cv2.imwrite("different_frames/" + "D" + str(filename) + '/' + str(filenameception), first_im_read)
+                        cv2.imwrite("different_frames/" + "D" + str(filename) + '/' + str(filenameception), new_im)
