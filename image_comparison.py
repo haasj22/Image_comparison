77be81deb58d814180890431f4a8110df2c68256
haasj22 <49049551+haasj22@users.noreply.github.com>
2020-10-20 09:20:17 -0700

    Add files via upload

--- image_comparison.py
+++ image_comparison.py
@@ -6,25 +6,25 @@ import PIL
 from PIL import Image
 import os
 
-for filename in os.listdir('cropped_frames'):
+for filename in os.listdir('new_cropped_frames'):
     first_im_read=None
     if not filename.startswith('.'):
-        for filenameception in os.listdir('cropped_frames/' + str(filename)):
+        for filenameception in os.listdir('new_cropped_frames/' + str(filename)):
             if filenameception.endswith('.jpg'):
                 if(first_im_read is None):
-                    first_im_read = cv2.imread("cropped_frames/" + str(filename) + '/' + str(filenameception), 0)
+                    first_im_read = cv2.imread("new_cropped_frames/" + str(filename) + '/' + str(filenameception))
                     print("First" + str(first_im_read))
-                    if not os.path.exists('different_frames'):
-                        os.makedirs('different_frames')
-                    if not os.path.exists("different_frames/" + "D" + str(filename)):
-                        os.makedirs("different_frames/" + "D" + str(filename))
-                    #cv2.imwrite("different_frames/" + "D" + str(filename) + '/' + str(filenameception), first_im_read)
+                    if not os.path.exists('new_different_frames'):
+                        os.makedirs('new_different_frames')
+                    if not os.path.exists("new_different_frames/" + str(filename)):
+                        os.makedirs("new_different_frames/" + str(filename))
+                    #cv2.imwrite("different_frames/" + str(filename) + '/' + str(filenameception), first_im_read)
                 else:
                     print("First" + str(first_im_read))
-                    new_im = cv2.imread("cropped_frames/" + str(filename) + '/' + str(filenameception), 0)
-                    print("cropped_frames/" + str(filename) + '/' + str(filenameception))
+                    new_im = cv2.imread("new_cropped_frames/" + str(filename) + '/' + str(filenameception))
+                    print("new_cropped_frames/" + str(filename) + '/' + str(filenameception))
                     print("Second" + str(new_im))
-                    error = measure.compare_ssim(first_im_read, new_im)
+                    error = measure.compare_ssim(first_im_read, new_im, multichannel=True)
                     print(error)
                     if error < 0.75:
-                        cv2.imwrite("different_frames/" + "D" + str(filename) + '/' + str(filenameception), new_im)
+                        cv2.imwrite("new_different_frames/" + str(filename) + '/' + str(filenameception), new_im)
